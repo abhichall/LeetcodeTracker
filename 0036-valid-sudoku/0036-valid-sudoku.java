366d4c10f169bdd9c9149c4d0e48943bd69be83c
Abhi Challa <158411574+abhichall@users.noreply.github.com>
2025-01-31 14:27:42 -0500

    Time: 15 ms (12.67%), Space: 44.8 MB (42.38%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,24 +1,25 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-
-        Set<String> hset= new HashSet<>();
+        
+        Set<String> hset = new HashSet<>();
 
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board.length; j++) {
 
                 char val = board[i][j];
 
-                if (val != '.') {
-                    
-                    if( !hset.add(val + "at row" + i) || !hset.add(val + "at col" + j) || !hset.add(val + "in box" + i/3 + j/3)){
-                        return false;
-                    }
+            if (val != '.') {
 
+                if( !hset.add(val + "at row" + i) || !hset.add(val + "at col" + j) || !hset.add(val + "at box" + i/3 + j/3) ) {
+
+                    return false;
                 }
 
             }
         }
+        }
 
-     return true;   
+        return true;
+
     }
 }
