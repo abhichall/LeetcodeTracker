76a012d0d2ba288499bb331cd46a8dbe53530ca5
Abhi Challa <158411574+abhichall@users.noreply.github.com>
2025-01-31 13:32:46 -0500

    Time: 11 ms (91.24%), Space: 50.1 MB (6.21%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,45 +1,51 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
         
-        // Create hashmap to store values and the frequency of each value
+        //hashmap
         Map<Integer, Integer> hmap = new HashMap<>();
 
-        // Add the values and frequencies to the hashmap
+        //populaate hmap with occ
         for (int n : nums) {
             hmap.put(n, hmap.getOrDefault(n, 0) + 1);
         }
 
-        // Create an array to hold a list of numbers at each frequency
-        // The frequency of values can be from 0 to the max number in the array, so use length + 1
-        List<Integer>[] freq = new List[nums.length + 1];
-        for (int i = 0; i < freq.length; i++) {
-            freq[i] = new ArrayList<>();
+        //create an array for the bucket sort values
+        List<Integer>[] arr = new List[nums.length + 1];
+
+        //populate the arr w arraylists
+        for(int i = 0; i < arr.length; i++) {
+            arr[i] = new ArrayList<>();
         }
 
-        // Populate the frequency array by iterating through each pair in the hashmap
+        //iterate through hashmap
         for (Map.Entry<Integer, Integer> pairs : hmap.entrySet()) {
-            int frequency = pairs.getValue();
-            freq[frequency].add(pairs.getKey());
+            
+            int index = pairs.getValue();
+
+            arr[index].add(pairs.getKey());
         }
 
-        // Create an int array to store the answer
-        int[] arr = new int[k];
 
-        // Counter to stop after k elements
+        //finallly for the answer
+
+        int[] ans = new int[k];
         int counter = 0;
 
-        // Since more frequent values are at the rightmost, iterate from the end
-        for (int i = freq.length - 1; i >= 0; i--) {
+        for (int i = arr.length - 1; i >= 0 ; i--) {
 
-            for (int n : freq[i]) {
-                arr[counter] = n;
+            for(int n : arr[i]) {
+
+                ans[counter] = n;
                 counter++;
 
-                // Return the array once k elements are added
-                if (counter == k) return arr;
+                if(counter == k) return ans;
             }
+            
+
         }
 
-        return arr;
+    return new int []{};
+
+        
     }
 }
