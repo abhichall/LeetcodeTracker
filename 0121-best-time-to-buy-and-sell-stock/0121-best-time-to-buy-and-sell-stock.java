f0ab5c24738154919cb3837f158ae35effcf78b5
Abhi Challa <158411574+abhichall@users.noreply.github.com>
2025-02-01 17:44:23 -0500

    Time: 1 ms (99.84%), Space: 61.8 MB (39.83%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,44 +1,26 @@
 class Solution {
     public int maxProfit(int[] prices) {
 
-        //price[i] is price on day i
-        //must buy before sell - cannot sort - and have to process in current order of the array
+        int currProfit = 0;
 
-        //clarification questions
-            // Can a stock be sold for 0?
-            // Can a price be negative
-            //Will there every be a carse when theire is only 1 value in the array
+        int lowestPrice = Integer.MAX_VALUE;
 
-        //brute force:
-            //nested for loop - start at index 0, looking for bigger values and return max
-            //do for whole loop
+        int maxProfit = 0;
 
-            //keep a comparison for max
-            int maxProfit = 0;
 
-            //curr profit tracker
-            int curr = 0;
+        for( int price : prices) {
 
-            //to find a min 
-            int min = Integer.MAX_VALUE;
-
-        //for loop - keep track of a min price, if the new value is greater than the min price, find profit -- because we buy at min, and sell if higher
-        for (int i = 0; i < prices.length; i++) {
-
-            //if its a min, no point in calculating
-            if (prices[i] < min) {
-                min = prices[i];
+            //set min
+            if(price < lowestPrice) {
+                lowestPrice = price;
+                continue;
             } else {
-                //find the profit
-                curr = prices[i] - min;
+                currProfit = price - lowestPrice;
             }
-        
 
-            //keep track of max
-            maxProfit = Math.max(curr, maxProfit);
+            maxProfit = Math.max(currProfit, maxProfit);
         }
 
-        return maxProfit;
-
+        return maxProfit;        
     }
 }
