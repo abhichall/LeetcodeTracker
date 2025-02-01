e05753ad01a222d51646e34e9c1d12effc54e4f2
Abhi Challa <158411574+abhichall@users.noreply.github.com>
2025-01-31 19:12:42 -0500

    Time: 2 ms (97.65%), Space: 42.2 MB (15.18%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,29 +1,29 @@
 class Solution {
     public boolean isValid(String s) {
 
-        //stack because we need to see the most recent addition in roder to check for validity
         Stack<Character> stack = new Stack();
 
-        //so this can be iterated through
         char[] arr = s.toCharArray();
 
         for (char c : arr) {
-            if (c == '(' || c == '{' || c == '[') {
+
+            if ( c == '(' || c == '[' || c == '{') {
                 stack.push(c);
             }
 
-            if(stack.isEmpty()) return false;
+            if (stack.isEmpty()) return false;
 
             if ( c == ')' && stack.pop() != '(') return false;
-            if ( c == '}' && stack.pop() != '{') return false;
             if ( c == ']' && stack.pop() != '[') return false;
-
+            if ( c == '}' && stack.pop() != '{') return false;
 
         }
 
-        if (stack.isEmpty()) return true;
-        return false;
-
+        if(stack.isEmpty()) {
+            return true;
+        } else {
+            return false;
+        }
         
     }
 }
