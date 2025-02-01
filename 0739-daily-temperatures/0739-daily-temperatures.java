caa5813f743c9f98cebce29ca2f560ade0c84451
Abhi Challa <158411574+abhichall@users.noreply.github.com>
2025-01-31 19:58:00 -0500

    Time: 64 ms (52.01%), Space: 63.5 MB (5.41%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.java
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -1,27 +1,23 @@
 class Solution {
     public int[] dailyTemperatures(int[] temperatures) {
 
-        //length
-        int n = temperatures.length;
+        int[] ans = new int[temperatures.length];
 
-        //int array for answer
-        int[] answer = new int[n];
-
-        //we can use a stack since we only care about next largest
         Stack<Integer> stack = new Stack();
 
-        //we can store indexes within the stack
-        for(int i = 0; i < n; i++) {
+        for(int i = 0; i < temperatures.length; i++) {
+
 
             while(!stack.isEmpty() && temperatures[i] > temperatures[stack.peek()]) {
                 int index = stack.pop();
-                answer[index] = i - index;
+                ans[index] = i - index;
             }
 
             stack.push(i);
 
         }
 
-        return answer;
+        return ans;
+        
     }
 }
