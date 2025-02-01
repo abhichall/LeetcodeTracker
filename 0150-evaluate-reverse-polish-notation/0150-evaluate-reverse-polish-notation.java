432ae2c82fa6761f3500d601b74a954586ac794a
Abhi Challa <158411574+abhichall@users.noreply.github.com>
2025-01-31 19:43:40 -0500

    Time: 6 ms (75.3%), Space: 44.9 MB (41.75%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -1,51 +1,32 @@
 class Solution {
     public int evalRPN(String[] tokens) {
 
-        //push onto stack until an operator
-        // then pop last two numbers
-        //perform related operation
-        //push back onto stack 
-
-        //create a stack of numbers
         Stack<Integer> stack = new Stack();
 
-        //go thru each string within the array
         for (String s : tokens) {
-            
-            //this would indicate an operation needed, if not then j push it on stack
-            if( s.equals("+") || s.equals("-") || s.equals("/") || s.equals("*") ) {
 
-            //the integer values
-            int x = stack.pop();
-            int y = stack.pop();
+            if (s.equals("+") || s.equals("-") || s.equals("*") || s.equals("/")) {
 
-            //do the operation and push the restul back onto the stack
+                int x = stack.pop();
+                int y = stack.pop();
+                
                 if(s.equals("+")) {
-                    int sum = x+y;
-                    stack.push(sum);
+                    stack.push(x+y);
                 } else if (s.equals("-")) {
-                    int diff = y-x;
-                    stack.push(diff);
+                    stack.push(y-x);
                 } else if (s.equals("*")) {
-                    int mult = x * y;
-                    stack.push(mult);
+                    stack.push(x*y);
                 } else if (s.equals("/")) {
-                    int qu = y / x;
-                    stack.push(qu);
+                    stack.push(y/x);
                 }
 
             } else {
-            
-            //other than the operations, it will ALWAYS be in an int
-            //make the string type into an int
-            int num = Integer.parseInt(s);
-            //push this onto the stack
-            stack.push(num);
+
+        int num = Integer.parseInt(s);
+        stack.push(num);
         }}
 
-        //at the end, the final result will be in th stack
         return stack.pop();
-
         
     }
 }
