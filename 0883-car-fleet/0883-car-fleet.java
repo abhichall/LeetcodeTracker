2831f0689c06f826529a041e375a646b7ecc1fe6
Abhi Challa <158411574+abhichall@users.noreply.github.com>
2025-02-01 13:56:02 -0500

    Time: 81 ms (54.49%), Space: 57.7 MB (56.24%) - LeetHub

--- 0883-car-fleet/0883-car-fleet.java
+++ 0883-car-fleet/0883-car-fleet.java
@@ -1,33 +1,35 @@
 class Solution {
     public int carFleet(int target, int[] position, int[] speed) {
-
-        //create a stack, to store, and be able to look at cars in fleets prev
-        Stack<Double> stack = new Stack();
         
-        //lets create a 2d array so that we can store the pos/speed pairs together 
+        //return the number of fleets
+        //arrival time will determine fleets?
+        Stack<Double> stack = new Stack();
+
+        //wanna store pos and speed pairs
         int[][] speedPos = new int[position.length][2];
 
-        //we can populate the 2d array thru
         for(int i = 0; i < position.length; i++) {
             speedPos[i][0] = position[i];
             speedPos[i][1] = speed[i];
         }
 
-        //now we can sort this pairs from least to greatest using a lambda function
-        Arrays.sort(speedPos, Comparator.comparingInt(element -> element[0]));
+        //sort by position
+        Arrays.sort(speedPos, (a,b) -> Integer.compare(a[0], b[0]));
 
-    //now that array is sorted - can conlude that if car y is behind car x, but car y has a shorter time to reach target than car x, then car y and car x will become part of the same fleet
 
-    //so we iterate from right, and push onto stack, if time of curr is <= stack.peek then same fleet
 
     for(int i = speedPos.length - 1; i >= 0; i--) {
 
         double time = (double) (target - speedPos[i][0]) / speedPos[i][1];
-        
-        if(!stack.isEmpty() && time <= stack.peek()) {
-            continue;
-        }
+
+
+        if(!stack.isEmpty() && time <= stack.peek()) continue;
+
         stack.push(time);
+
+        //if times are equal = same fleet
+        //if car x is ahead of car y, but car y has less time to target, same flee
+
     }
 
     return stack.size();
