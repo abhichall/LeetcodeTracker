c6fdcaf12fcd0f4ed327d826fa6c70833860c147
Abhi Challa <158411574+abhichall@users.noreply.github.com>
2025-02-02 20:27:18 -0500

    Time: 9 ms (50.42%), Space: 42.4 MB (97.41%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,34 +1,39 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
 
-        if(s1.length() > s2.length()) return false;
+        //make a array of char occ
+        //slide it through longer one
+        //if equal, then the return true
 
-        //keep track of frequency within size of s1 window
-        int[] s1Count = new int[26];
-        int[] s2Count = new int[26];
+        int[] s1occ = new int[26];
+        int[] s2occ = new int[26];
+        
+
+        //edge cases
+        if (s1.length() > s2.length()) return false;
 
         for(int i = 0; i < s1.length(); i++) {
-            s1Count[s1.charAt(i) - 'a']++;
-            s2Count[s2.charAt(i) - 'a']++;
+            s1occ[s1.charAt(i) - 'a']++;
+            s2occ[s2.charAt(i) - 'a']++;
         }
 
-        //now if the arrays are equal then true
-        if(Arrays.equals(s1Count, s2Count)) return true;
+        if(Arrays.equals(s1occ, s2occ)) return true;
 
+        
         for(int i = s1.length(); i < s2.length(); i++) {
 
-            //add this value
-            s2Count[s2.charAt(i) - 'a']++;
+            s2occ[s2.charAt(i) - 'a']++;
+            
+            s2occ[s2.charAt(i - s1.length()) - 'a']--;
 
-            //remove first val
-            s2Count[s2.charAt(i - s1.length()) - 'a']--;
-
-            if(Arrays.equals(s1Count, s2Count)) return true;
-
-
+        
+            if(Arrays.equals(s1occ, s2occ)) return true;
 
         }
 
-     return false;   
+
+        return false;
+
+        
     }
 }
