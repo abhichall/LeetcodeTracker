43a8bc9c9ddac15f1c1d7cae0f12614ebc4980db
Abhi Challa <158411574+abhichall@users.noreply.github.com>
2025-02-07 12:20:08 -0500

    Time: 8 ms (39.95%), Space: 48.1 MB (44.52%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,24 +1,31 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        
+
+        //what happens if str was empty
+        //walk through given test cases - make own test cases to try for understaind
+
+        //hashmap to store sorted and then the corresponding vals
         Map<String, List<String>> hmap = new HashMap<>();
 
-        for (String s : strs) {
+        // sorted val : values
 
-            char[] arr = s.toCharArray();
+        for(String s : strs) {
 
-            Arrays.sort(arr);
+            char[] sort = s.toCharArray();
+            Arrays.sort(sort);
+            String key = new String(sort);
 
-            String upd = new String(arr);
-
-            if(hmap.containsKey(upd)) {
-                hmap.get(upd).add(s);
+            if(hmap.containsKey(key)) {
+                hmap.get(key).add(s);
             } else {
-                hmap.put(upd, new ArrayList<>());
-                hmap.get(upd).add(s);
+                hmap.put(key, new ArrayList<>());
+                hmap.get(key).add(s);
             }
+
         }
+        
 
         return new ArrayList<>(hmap.values());
+        
     }
 }
