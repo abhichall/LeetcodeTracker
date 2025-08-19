e7d1cd4fd3258ba4d5e65afb1be4dab76866bcd3
Abhi Challa <158411574+abhichall@users.noreply.github.com>
2025-08-19 14:34:34 -0400

    Time: 11 ms (91.87%), Space: 49.9 MB (5.07%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,39 +1,39 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
         
-        //hashmap
+        //hmap to keep track of occurences of each value
         Map<Integer, Integer> hmap = new HashMap<>();
-
-        //populaate hmap with occ
+        
+        //populate hmap with occurences
         for (int n : nums) {
             hmap.put(n, hmap.getOrDefault(n, 0) + 1);
         }
 
-        //create an array for the bucket sort values
-        List<Integer>[] arr = new List[nums.length + 1];
+        //create an array list to store the keys in relation the the occurence value
 
-        //populate the arr w arraylists
-        for(int i = 0; i < arr.length; i++) {
-            arr[i] = new ArrayList<>();
+        //create array of size nums populate with array lists
+        //array for bucket sort
+        List<Integer>[] buckets = new ArrayList[nums.length + 1];
+
+        for(int i = 0; i < buckets.length; i++) {
+            buckets[i] = new ArrayList<>();
         }
 
-        //iterate through hashmap
-        for (Map.Entry<Integer, Integer> pairs : hmap.entrySet()) {
-            
-            int index = pairs.getValue();
+      
 
-            arr[index].add(pairs.getKey());
+            //populate each index (occ) with the key (nums)
+        for (Map.Entry<Integer, Integer> pair : hmap.entrySet()) {
+
+            int index = pair.getValue();
+            buckets[index].add(pair.getKey());
+
         }
+        
+    int[] ans = new int[k];
+    int counter = 0;
+       for (int i = buckets.length - 1; i >= 0 ; i--) {
 
-
-        //finallly for the answer
-
-        int[] ans = new int[k];
-        int counter = 0;
-
-        for (int i = arr.length - 1; i >= 0 ; i--) {
-
-            for(int n : arr[i]) {
+            for(int n : buckets[i]) {
 
                 ans[counter] = n;
                 counter++;
