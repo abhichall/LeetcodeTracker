2e2c3833a58a7df1d61c7223899ff9919782992d
Abhi Challa <158411574+abhichall@users.noreply.github.com>
2025-09-04 01:44:35 -0400

    Time: 0 ms (100%), Space: 42.5 MB (42.7%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -1,26 +1,38 @@
 class Solution {
     public int findPeakElement(int[] nums) {
-        int left = 0;
-        int right = nums.length - 1;
-
-        while (left <= right) {
+        int left = 0, right = nums.length - 1;
+        
+        while (left < right) {
             int mid = left + (right - left) / 2;
-
-            // Check if mid is a peak element
-            if ((mid == 0 || nums[mid] > nums[mid - 1]) && (mid == nums.length - 1 || nums[mid] > nums[mid + 1])) {
-                return mid;
-            }
-            // If the left neighbor is greater, search in the left half
-            else if (mid > 0 && nums[mid] < nums[mid - 1]) {
-                right = mid - 1;
-            }
-            // If the right neighbor is greater, search in the right half
-            else {
+            
+            if (nums[mid] < nums[mid + 1]) {
+                // Peak must be on the right
                 left = mid + 1;
+            } else {
+                // Peak is on the left side or at mid
+                right = mid;
             }
         }
-
-        // In case of no peaks (not possible for this problem with the given constraints)
-        return -1;
+        
+        return left; // or right, they meet at the peak
     }
 }
+
+/**
+
+nums = [1 2 3 1]
+output = 2
+3 is peak elemnent -- index is 2
+
+
+binary search 
+
+    the peak cant be the first value or last value
+    must be between
+    if we find where nums[i] < nums[i + 1] we know that some sort fo peak exists on the right side (since peak must be the greatest when compared to surroundings)
+    and vice versa
+
+    [1 2 3 1]
+
+
+ */
