85beb59e84dafe02875916554e5fc7e9dbc05f2b
Abhi Challa <158411574+abhichall@users.noreply.github.com>
2025-09-13 09:45:39 -0400

    Time: 1 ms (99.73%), Space: 55.5 MB (62.08%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,31 +1,24 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
 
-        int size = nums.length;
-        int[] ans = new int[size];
+        //create an int array for answer
+        int[] answer = new int[nums.length];
 
-
-        //prefix
-        int[] left = new int[size];
-
-        //postfix
-        int[] right = new int[size];
-
-        //left most and right most will b 1 since there is no val outside
-        left[0] = 1;
-        right[size - 1] = 1;
-
-        for (int i = 1; i < nums.length; i++) {
-            left[i] = left[i-1] * nums[i-1];
-        }
-
-        for (int i = size - 2; i >= 0; i--) {
-            right[i] = right[i+1] * nums[i+1];
-        }
+        //for loops to iterate through the array
         
-        for (int i = 0; i < nums.length; i++) {
-            ans[i] = left[i] * right[i];
-        }
-return ans;
+            //for left multiples, start with left = 1 because the left of the left most should be 1
+            for(int i = 0, left = 1; i < nums.length; i++) {
+                answer[i] = left;
+                left = left * nums[i];
+            }
+            //after this loop above, all the values in the answer array are now the product of all the left values of each index
+
+            //now same thing for the right
+            for(int i = nums.length - 1, right = 1; i >= 0; i--) {
+                
+                answer[i] = right * answer[i];
+                right = right * nums[i];
+            }
+        return answer;
     }
 }
