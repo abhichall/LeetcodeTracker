d3cee62149e63041dfbc0ed526a3dbbb874f7793
Abhi Challa <158411574+abhichall@users.noreply.github.com>
2025-10-12 14:09:28 -0400

    Time: 6 ms (27%), Space: 44.5 MB (66.26%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -10,32 +10,47 @@
  */
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-        
-        //check if null or not anything
         if (lists == null || lists.length == 0) return null;
 
-        PriorityQueue<ListNode> minHeap = new PriorityQueue<>((a,b) -> a.val - b.val);
+        //min heap order by node val
+        PriorityQueue<ListNode> minHeap = new PriorityQueue<>(Comparator.comparingInt(n -> n.val));
 
-        //linkedlist- only stores head so each node is first
-        for(ListNode node : lists) {
-            if (node != null) minHeap.add(node);
+        for (ListNode node : lists) {
+            if (node != null) minHeap.offer(node);
         }
 
+        //dummy pointers
         ListNode dummy = new ListNode(0);
-        ListNode ptr = dummy;
+        ListNode tail = dummy;
 
+        //while present in heap, popo and append
         while(!minHeap.isEmpty()) {
-            ListNode small = minHeap.poll();
-            ptr.next = small;
-            ptr = ptr.next;
-        
-            if (small.next != null) {
-                minHeap.add(small.next);
+            ListNode smallest = minHeap.poll(); //will hold the smallest
+            tail.next = smallest; //append to the merged list
+            tail = tail.next;   //move the tail
+
+            //if this head has a next value then we push that to heap
+            if (smallest.next != null) {
+                minHeap.offer(smallest.next);
             }
-        
+
+            
         }
-
         return dummy.next;
-    }
-
+    
 }
+}
+/*
+IDEAS
+    get all the avlues of the nodes into an array
+    sort
+    write this into a linked list 
+    Onlogn -- bad efficiency
+
+    needs to be ascending 
+    pushed into a min heap -- o logn
+
+    have heap initialized with the head of each list
+    then go into that list when we do access it
+
+*/
