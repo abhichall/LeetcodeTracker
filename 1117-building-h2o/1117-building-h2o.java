d0a386d03e46351bba3091cdda0ac65f96f3beff
Abhi Challa <158411574+abhichall@users.noreply.github.com>
2026-01-06 21:36:19 -0500

    Time: 16 ms (96.39%), Space: 47.8 MB (19.79%) - LeetHub

--- 1117-building-h2o/1117-building-h2o.java
+++ 1117-building-h2o/1117-building-h2o.java
@@ -1,31 +1,31 @@
-class H2O {
-    private Semaphore hydrogenSem = new Semaphore(2);
-    private Semaphore oxygenSem = new Semaphore(1);
-    private CyclicBarrier barrier = new CyclicBarrier(3, () -> {
-        // Reset semaphores after forming one water molecule
-        hydrogenSem.release(2);
-        oxygenSem.release(1);
-    });
+/**
+
+switch between the hydrogen and the oxygen 
+
+    semaphore for hydrogen - 2
+    semaphore for oxygen - 0
+ */
+
+ class H2O {
+
+    private Semaphore hSem = new Semaphore(2);
+    private Semaphore oSem = new Semaphore(0);
 
     public H2O() {}
 
-    public void hydrogen(Runnable releaseHydrogen) throws InterruptedException {
-        hydrogenSem.acquire();
-        try {
-            releaseHydrogen.run(); // outputs "H"
-            barrier.await(); // wait for 2H + 1O
-        } catch (Exception e) {
-            Thread.currentThread().interrupt();
-        }
+    public void hydrogen (Runnable releaseHydrogen) throws InterruptedException {
+        hSem.acquire(); //wait for hydrogen to be open run
+        releaseHydrogen.run();
+
+        if (hSem.availablePermits() == 0) oSem.release(); //signal oSem to start
     }
 
-    public void oxygen(Runnable releaseOxygen) throws InterruptedException {
-        oxygenSem.acquire();
-        try {
-            releaseOxygen.run(); // outputs "O"
-            barrier.await(); // wait for 2H + 1O
-        } catch (Exception e) {
-            Thread.currentThread().interrupt();
-        }
+    public void oxygen (Runnable releaseOxygen) throws InterruptedException {
+        oSem.acquire(); //wait until 2 hydrogens are ready
+        releaseOxygen.run();
+        hSem.release(2);
+
     }
-}
+
+
+ }
